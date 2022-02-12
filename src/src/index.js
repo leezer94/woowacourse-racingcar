512eb3fa4991c79c1bb2a76c7ab545b6088b6fd2
leezer94 <2kunhee94@gmail.com>
2022-02-12 15:01:40 +0900

    Feat: 추가 입력된 자동차 input value 유효성 검사 기능

---
+++ src/src/index.js
@@ -0,0 +1,50 @@
+import { $, $$ } from './utils/selector.js';
+import { carPlayerTemplate, carForwardTemplate } from './utils/template.js';
+import { getRandomNumber } from './utils/utils.js';
+
+class RacingCar {
+  constructor() {
+    this.$ = {
+      carNameInput: $('#car-name-input'),
+      carNameSubmit: $('#car-name-submit'),
+      racingCountInput: $('#racing-count-input'),
+      racingCountSubmit: $('#racing-count-submit'),
+      racingCarElement: $('.racing-car-container'),
+    };
+
+    this.bindEventHandlers();
+  }
+
+  // Car name related functions
+
+  getCarNameInputValue(input) {
+    const { value } = input;
+
+    return value.split(',');
+  }
+
+  isValidInput(inputValue) {
+    let isValid = true;
+
+    inputValue = inputValue.forEach((value) => {
+      if (value.length > 5) {
+        isValid = false;
+        alert('5 자 이하의 자동차 이름을 입력해 주십시요.');
+      } else if (!value) {
+        isValid = false;
+        alert('자동차 이름을 입력해 주십시오.');
+      } else if (value.includes(' ')) {
+        isValid = false;
+        alert('자동차 이름은 공백을 포함해서는 안됩니다.');
+      }
+    });
+
+    return isValid;
+  }
+
+  bindEventHandlers() {
+    this.$.carNameSubmit.addEventListener('click', () => {});
+  }
+}
+
+new RacingCar();
